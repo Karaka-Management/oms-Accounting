2d304ea5a98071c16310b5c9013769291f43dc0e
Dennis Eichhorn <spl1nes.com@googlemail.com>
2019-11-10 22:40:27 +0100

    cleanup tests and use @covers more

--- Models/PostingAbstract.php
+++ Models/PostingAbstract.php
@@ -25,12 +25,4 @@ namespace Modules\Accounting\Models;
 abstract class PostingAbstract implements PostingInterface
 {
 
-    /**
-     * Constructor.
-     *
-     * @since 1.0.0
-     */
-    public function __construct()
-    {
-    }
 }
