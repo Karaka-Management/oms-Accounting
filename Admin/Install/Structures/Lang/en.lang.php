ba3f62ed6731b3f54ccd2b9af79e4679fff35c67
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-10-03 09:27:46 +0200

    fix class names and typos

--- Admin/Install/Structures/Lang/en.lang.php
+++ Admin/Install/Structures/Lang/en.lang.php
@@ -1,5 +1,15 @@
 <?php
-
+/**
+ * Orange Management
+ *
+ * PHP Version 8.0
+ *
+ * @package   Modules\Accounting\Admin\Install
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
 declare(strict_types=1);
 
 return [
