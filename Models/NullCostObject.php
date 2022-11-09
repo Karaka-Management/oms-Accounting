e565b9b6a9a39e856e8e9adfd1d39126d6fdbb23
Dennis Eichhorn <spl1nes.com@googlemail.com>
2022-11-09 23:02:52 +0100

    reduce serialization of null models

--- Models/NullCostObject.php
+++ Models/NullCostObject.php
@@ -35,4 +35,12 @@ final class NullCostObject extends CostObject
     {
         $this->id = $id;
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function jsonSerialize() : mixed
+    {
+        return ['id' => $this->id];
+    }
 }
