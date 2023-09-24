3ff0f4f8a51115974b7b9caf140979ba196a2af1
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-09-24 17:57:27 +0000

    fix tests

--- tests/Admin/AdminTest.php
+++ tests/Admin/AdminTest.php
@@ -23,5 +23,5 @@ final class AccountingTest extends \PHPUnit\Framework\TestCase
 
     protected const URI_LOAD = 'http://127.0.0.1/en/backend/accounting';
 
-    use \Modules\tests\ModuleTestTrait;
+    use \Build\Helper\ModuleTestTrait;
 }
