f3830c7f8f92fae88a9fbed43e10072d6d85b42c
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-10-17 00:13:58 +0000

    fix jsonSerialize function call

--- tests/Models/NullCostCenterTest.php
+++ tests/Models/NullCostCenterTest.php
@@ -47,6 +47,6 @@ final class NullCostCenterTest extends \PHPUnit\Framework\TestCase
     public function testJsonSerialize() : void
     {
         $null = new NullCostCenter(2);
-        self::assertEquals(['id' => 2], $null);
+        self::assertEquals(['id' => 2], $null->jsonSerialize());
     }
 }
